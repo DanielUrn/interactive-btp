d5efc878e9d3aee9b201b44c668486261e9faee8
Julio Graterol <juliograterolb@gmail.com>
2023-08-08 11:41:31 -0400

    Added Tawk_API.toggle to chat.js

--- functions/chat.js
+++ functions/chat.js
@@ -3,5 +3,8 @@ const chatBtn = document.getElementById('tawkBtn')
 chatBtn.addEventListener('click', ()=>{
     if(widget.length > 0){
         widget[0].setAttribute('class', 'widget-visible')
+        if(typeof Tawk_API !== undefined){
+            Tawk_API.toggle()
+        }
     }
 })
