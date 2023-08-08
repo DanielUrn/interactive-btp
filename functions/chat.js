87cf9d7a585ab9d0f500d77fb4c35b139882db0d
Julio Graterol <juliograterolb@gmail.com>
2023-08-08 11:48:52 -0400

    Added Tawk_API.toggle to chat.js up, down buttons

--- functions/chat.js
+++ functions/chat.js
@@ -1,10 +1,19 @@
-const chatBtn = document.getElementById('tawkBtn')
+const upBtn = document.getElementById('tawkBtn-up')
+const downBtn = document.getElementById('tawkBtn-down')
 
-chatBtn.addEventListener('click', ()=>{
+upBtn.addEventListener('click', ()=>{
+    chatNow()
+})
+
+downBtn.addEventListener('click', ()=>{
+    chatNow()
+})
+
+function chatNow(){
     if(widget.length > 0){
         widget[0].setAttribute('class', 'widget-visible')
     }
     if(typeof Tawk_API !== undefined){
         Tawk_API.toggle()
     }
-})
+}
