7d26613f9c2207d4eb9fce9af9133ffec703865b
Julio Graterol <juliograterolb@gmail.com>
2023-08-08 12:28:14 -0400

    html updated href in navbar

--- functions/chat.js
+++ functions/chat.js
@@ -1,5 +1,7 @@
 const upBtn = document.getElementById('tawkBtn-up')
 const downBtn = document.getElementById('tawkBtn-down')
+const shown = document.getElementsByClassName('widget-visible')
+var tawkPanel = document.getElementsByClassName('open')
 
 upBtn.addEventListener('click', ()=>{
     chatNow()
@@ -15,5 +17,8 @@ function chatNow(){
     }
     if(typeof Tawk_API !== undefined){
         Tawk_API.toggle()
+        if(tawkPanel.length > 0 && tawkPanel[0].className === 'open'){   
+            shown[0].setAttribute('class', 'tawk-widget-visible')
+        }
     }
 }
